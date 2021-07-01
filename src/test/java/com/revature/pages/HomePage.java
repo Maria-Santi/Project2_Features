9569196772de4d109f3da6d14f1d7e4fb95278bb
dboudreau4 <david.boudreau4@gmail.com>
2021-07-01 14:22:35 -0700

    create account, login, and homepage features

--- src/test/java/com/revature/pages/HomePage.java
+++ src/test/java/com/revature/pages/HomePage.java
@@ -1,4 +1,22 @@
 package com.revature.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "/html/body/nav/div/ul/li[1]/a")
+    public WebElement createAccount;
+
+    @FindBy(xpath = "/html/body/nav/div/ul/li[2]/a")
+    public WebElement login;
+
 }
